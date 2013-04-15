f3ab0652498e6392490c208941fd9a624e48cb63
minrivertea <chris@minrivertea.com>
2013-04-15 21:44:35 +0800

    500 template

--- context_processors.py
+++ context_processors.py
@@ -9,7 +9,7 @@ from website.models import Page
 
 def common(request):
     context = {}
-    context['navigation'] = Page.objects.filter(main_navigation=True).order_by(-'main_navigation_position')
+    context['navigation'] = Page.objects.filter(main_navigation=True).order_by('-main_navigation_position')
     context['base_template'] = settings.BASE_TEMPLATE
     context['site_name'] = settings.SITE_NAME
     
